7bfe26d2863955d7acb37800c8de615c6a8307c5
Zeljko Lucic <zeljko.lucic@levi9.com>
2022-07-11 08:44:59 +0200

    Initial Commit

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Željko Lučić on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
