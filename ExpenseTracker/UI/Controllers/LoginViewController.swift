5ac32d55d2a843cf7edaf6b8da6283de05320811
Zeljko Lucic <zeljko.lucic@levi9.com>
2022-07-12 09:19:21 +0200

    Implement LoginViewController layout

---
+++ ExpenseTracker/UI/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  ExpenseTracker
+//
+//  Created by Željko Lučić on 7/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           
+        
+    }
+    
+}
