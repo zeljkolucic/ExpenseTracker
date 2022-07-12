ced19705411018dcc741374d52887a1e33401250
Zeljko Lucic <zeljko.lucic@levi9.com>
2022-07-12 10:01:05 +0200

    Implement FirstStepRegistrationViewController layout

---
+++ ExpenseTracker/UI/Controllers/FirstStepRegistrationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstStepRegistrationViewController.swift
+//  ExpenseTracker
+//
+//  Created by Željko Lučić on 7/12/22.
+//
+
+import UIKit
+
+class FirstStepRegistrationViewController: UIViewController {
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
