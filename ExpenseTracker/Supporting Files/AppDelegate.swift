5a4dd2b0f2d7fa4cc9bde8bf915f25ae0a54b362
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-09-13 00:20:52 +0200

    Integrate IQKeyboardManager

--- ExpenseTracker/Supporting Files/AppDelegate.swift
+++ ExpenseTracker/Supporting Files/AppDelegate.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
@@ -21,11 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
-    }
-
 
 }
 
